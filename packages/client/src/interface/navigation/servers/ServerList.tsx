66291bf5bbda6ed41be26254cc78e1254cb5bca2
meijerivoi <themeijerivoi@gmail.com>
2026-03-07 12:23:59 +0200

    fix: hide alert if server is muted (#849)
    * fix: hide alert if server is muted
    
    Signed-off-by: meijerivoi <themeijerivoi@gmail.com>
    
    * fix: change & to &&
    
    Signed-off-by: meijerivoi <themeijerivoi@gmail.com>
    
    * fix: Prettier line length change
    
    Signed-off-by: meijerivoi <themeijerivoi@gmail.com>
    
    ---------
    
    Signed-off-by: meijerivoi <themeijerivoi@gmail.com>
    Signed-off-by: 3t6 <110821381+3T6-dsc@users.noreply.github.com>

--- packages/client/src/interface/navigation/servers/ServerList.tsx
+++ packages/client/src/interface/navigation/servers/ServerList.tsx
@@ -258,7 +258,8 @@ export const ServerList = (props: Props) => {
                   indicator:
                     props.selectedServer() === entry.item.id
                       ? "selected"
-                      : entry.item.unread
+                      : entry.item.unread &&
+                          !state.notifications.isMuted(entry.item)
                         ? "alert"
                         : undefined,
                 })}
