4a86ae013c7563bfc9ddffdaa53ded66915a77c2
Mechanically Stupid Medic <warmedic86@gmail.com>
2026-03-07 12:35:37 +0200

    fix: Make ProfileJoined.tsx adhere to locale (#972)
    * fix: Make ProfileJoined.tsx follow locale
    
    Fix: ProfileJoined.tsx hardcodes date format, make it adhere to locale instead
    
    Basic change, it's also my first time really contributing. I think I understand how the locale system in the app works but I've had a rough time figuring it out so apologies if I still got something wrong.
    
    I plan to have a go at fixing the rest of the issue but for now I'll just submit this.
    
    Partially Fixes: #811
    Signed-off-by: Medic-Kronos <warmedic86@gmail.com>
    
    * fix: Make ProfileJoined.tsx follow locale
    
    Fix: ProfileJoined.tsx hardcodes date format, make it adhere to locale instead
    
    Basic change, it's also my first time really contributing. I think I understand how the locale system in the app works but I've had a rough time figuring it out so apologies if I still got something wrong.
    
    I plan to have a go at fixing the rest of the issue but for now I'll just submit this.
    
    Partially Fixes: #811
    Signed-off-by: Medic-Kronos <warmedic86@gmail.com>
    
    * fix: Quickly format it in the way it was before
    
    Signed-off-by: Medic-Kronos <warmedic86@gmail.com>
    
    ---------
    
    Signed-off-by: Medic-Kronos <warmedic86@gmail.com>
    Co-authored-by: Mihai <45673304+mihaicm93@users.noreply.github.com>
    Signed-off-by: 3t6 <110821381+3T6-dsc@users.noreply.github.com>

--- packages/client/components/ui/components/features/profiles/ProfileJoined.tsx
+++ packages/client/components/ui/components/features/profiles/ProfileJoined.tsx
@@ -3,7 +3,7 @@ import { Show } from "solid-js";
 import { Trans } from "@lingui-solid/solid/macro";
 import { ServerMember, User } from "stoat.js";
 
-import { useTime } from "@revolt/i18n";
+import { timeLocale, useTime } from "@revolt/i18n";
 
 import { Text } from "../../design";
 import { OverflowingText } from "../../utils";
@@ -22,13 +22,27 @@ export function ProfileJoined(props: { user: User; member?: ServerMember }) {
         <OverflowingText>Stoat</OverflowingText>
         {/* <Trans>Account Created</Trans> */}
       </Text>
-      <Text>{dayjs(props.user.createdAt).format("DD MMM YYYY")}</Text>
+      <Text>
+        {dayjs(props.user.createdAt).format(
+          timeLocale()[1]
+            .formats.L?.replace("MM", "MMM")
+            .replaceAll("/", " ")
+            .replaceAll("-", " "),
+        )}
+      </Text>
       <Show when={props.member}>
         <Text class="label">
           <OverflowingText>{props.member!.server!.name}</OverflowingText>
           {/* <Trans>Member Since</Trans> */}
         </Text>
-        <Text>{dayjs(props.member!.joinedAt).format("DD MMM YYYY")}</Text>
+        <Text>
+          {dayjs(props.member!.joinedAt).format(
+            timeLocale()[1]
+              .formats.L?.replace("MM", "MMM")
+              .replaceAll("/", " ")
+              .replaceAll("-", " "),
+          )}
+        </Text>
       </Show>
     </ProfileCard>
   );
